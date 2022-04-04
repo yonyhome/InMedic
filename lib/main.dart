afd11a8f984f59c958fc106b68a6d650c5325f3f
Jose Angel Rodriguez Gonzalez <rodriguezgonzalez@uninorte.edu.co>
2022-04-03 21:11:48 -0500

    back a inicio

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'doctor.dart';
 import 'PantallaInicio.dart';
 
 void main() {
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Doctor(),
+      home: PantallaInicio(),
     );
   }
 }
